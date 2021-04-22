8d750ac811ee7c10f79ceace1d2737bb459a515e
Vitaliy Sulimenko <prevetal@gmail.com>
2021-04-22 15:29:30 +0300

    Переделанная главная, переделанная  О компании

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,66 @@
 $(() => {
 	// Основной слайдер на главной
 	$('.main_slider .slider').owlCarousel({
-		items           : 1,
-		margin          : 0,
-		nav             : true,
-		dots            : false,
-		loop            : true,
-		smartSpeed      : 750,
-		autoplay        : true,
-		autoplayTimeout : 5000,
-		onInitialized   : (event) => {
+		items: 1,
+		margin: 0,
+		nav: true,
+		dots: false,
+		loop: true,
+		smartSpeed: 750,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		onInitialized: (event) => {
 			event.item.count < 10
-				? $('.main_slider .count .total').text('0'+ event.item.count)
+				? $('.main_slider .count .total').text('0' + event.item.count)
 				: $('.main_slider .count .total').text(event.item.count)
-	    },
+		},
 		onTranslate: (event) => {
-	    	let currentIndex = event.item.index - event.relatedTarget._clones.length / 2
+			let currentIndex = event.item.index - event.relatedTarget._clones.length / 2
 
-	    	currentIndex < 0
-	    		? currentIndex = event.item.count
-	    		: currentIndex = currentIndex+1
+			currentIndex < 0
+				? currentIndex = event.item.count
+				: currentIndex = currentIndex + 1
 
-	    	if(currentIndex > event.item.count) {currentIndex = event.item.count}
+			if (currentIndex > event.item.count) { currentIndex = event.item.count }
 
-	    	event.item.count < 10
-				? $('.main_slider .count .current').text('0'+ currentIndex)
+			event.item.count < 10
+				? $('.main_slider .count .current').text('0' + currentIndex)
 				: $('.main_slider .count .current').text(currentIndex)
-	    }
+		}
+	})
+
+
+	// Инфо. блок
+	$('.info_block .slider').owlCarousel({
+		items: 1,
+		margin: 0,
+		nav: true,
+		dots: false,
+		loop: true,
+		smartSpeed: 750,
+		onInitialized: (event) => {
+			event.item.count < 10
+				? $('.info_block .count .total').text('0' + event.item.count)
+				: $('.info_block .count .total').text(event.item.count)
+		},
+		onTranslate: (event) => {
+			let currentIndex = event.item.index - event.relatedTarget._clones.length / 2
+
+			currentIndex < 0
+				? currentIndex = event.item.count
+				: currentIndex = currentIndex + 1
+
+			if (currentIndex > event.item.count) { currentIndex = event.item.count }
+
+			event.item.count < 10
+				? $('.info_block .count .current').text('0' + currentIndex)
+				: $('.info_block .count .current').text(currentIndex)
+		}
 	})
 
 
 	// Фиксация label при вводе в поле
-	$('body').on('keydown', '.form .input, .form textarea', function() {
+	$('body').on('keydown', '.form .input, .form textarea', function () {
 		let _self = $(this)
 
 		setTimeout(() => {
@@ -43,10 +72,10 @@ $(() => {
 
 
 	// Показать/Скрыть пароль
-	$('.form .view_btn').click(function(e){
+	$('.form .view_btn').click(function (e) {
 		e.preventDefault()
 
-		if(!$(this).hasClass('active')) {
+		if (!$(this).hasClass('active')) {
 			$(this).addClass('active')
 			$(this).closest('.field').find('.input').prop('type', 'text')
 		} else {
@@ -57,7 +86,7 @@ $(() => {
 
 
 	// Спойлер в характеристиках
-	$('body').on('click', '.features_block .list .spoler_link', function(e) {
+	$('body').on('click', '.features_block .list .spoler_link', function (e) {
 		e.preventDefault()
 
 		const $parent = $(this).closest('.list')
@@ -76,28 +105,28 @@ $(() => {
 
 	// Товар
 	$('.product_info .images .big .slider').owlCarousel({
-		items      : 1,
-		margin     : 20,
-		loop       : false,
-		smartSpeed : 500,
-		nav        : false,
-		responsive : {
-	        0: {
-				dots : true
-	        },
-	        480: {
-				dots : false
-	        }
+		items: 1,
+		margin: 20,
+		loop: false,
+		smartSpeed: 500,
+		nav: false,
+		responsive: {
+			0: {
+				dots: true
+			},
+			480: {
+				dots: false
+			}
 		},
-		onTranslate : (event) => {
+		onTranslate: (event) => {
 			const parent = $(event.target).closest('.images')
 
 			parent.find('.thumbs button').removeClass('active')
-			parent.find('.thumbs button:eq('+ event.item.index +')').addClass('active')
+			parent.find('.thumbs button:eq(' + event.item.index + ')').addClass('active')
 		}
 	})
 
-	$('.product_info .images .thumbs button').click(function(e) {
+	$('.product_info .images .thumbs button').click(function (e) {
 		e.preventDefault()
 
 		const parent = $(this).closest('.images')
@@ -108,33 +137,33 @@ $(() => {
 
 	// Календарь
 	$('.date_input').datepicker({
-		autoClose : true,
-		maxDate   : new Date()
+		autoClose: true,
+		maxDate: new Date()
 	})
 
 
 	// Отправка форм
-	$('body').on('submit', '#register_modal .form', function(e) {
+	$('body').on('submit', '#register_modal .form', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close()
 
 		$.fancybox.open({
-			src   : '#register_success_modal',
-			type  : 'inline',
-			touch : false
+			src: '#register_success_modal',
+			type: 'inline',
+			touch: false
 		})
 	})
 
-	$('body').on('submit', '#edit_info_modal .form', function(e) {
+	$('body').on('submit', '#edit_info_modal .form', function (e) {
 		e.preventDefault()
 
 		$.fancybox.close()
 
 		$.fancybox.open({
-			src   : '#edit_info_success_modal',
-			type  : 'inline',
-			touch : false
+			src: '#edit_info_success_modal',
+			type: 'inline',
+			touch: false
 		})
 	})
 
@@ -144,14 +173,7 @@ $(() => {
 
 	if ($('.how_it_work').length) {
 		inView('.how_it_work .items .item')
-			.on('enter', function(el) {
-				$(el).addClass('animate')
-			})
-	}
-
-	if ($('.info_block').length) {
-		inView('.info_block')
-			.on('enter', function(el) {
+			.on('enter', function (el) {
 				$(el).addClass('animate')
 			})
 	}
@@ -165,11 +187,15 @@ $(() => {
 
 $(window).on('load', () => {
 	// Выравнивание элементов в сетке
-	$('.advantages .row').each(function(){
+	$('.promo_products .row').each(function () {
+		promoProductsHeight($(this), parseInt($(this).css('--promo_products_count')))
+	})
+
+	$('.advantages .row').each(function () {
 		advantageHeight($(this), parseInt($(this).css('--advantages_count')))
 	})
 
-	$('.documents .row').each(function(){
+	$('.documents .row').each(function () {
 		documentHeight($(this), parseInt($(this).css('--documents_count')))
 	})
 })
@@ -178,46 +204,66 @@ $(window).on('load', () => {
 
 $(window).resize(() => {
 	// Выравнивание элементов в сетке
-	$('.advantages .row').each(function(){
+	$('.promo_products .row').each(function () {
+		promoProductsHeight($(this), parseInt($(this).css('--promo_products_count')))
+	})
+
+	$('.advantages .row').each(function () {
 		advantageHeight($(this), parseInt($(this).css('--advantages_count')))
 	})
 
-	$('.documents .row').each(function(){
+	$('.documents .row').each(function () {
 		documentHeight($(this), parseInt($(this).css('--documents_count')))
 	})
 })
 
 
+// Выравнивание преимуществ
+function promoProductsHeight(context, step) {
+	let start = 0,
+		finish = step,
+		$advantages = context.find('.product')
+
+	$advantages.find('.name, .desc').height('auto')
+
+	$advantages.each(function () {
+		setHeight($advantages.slice(start, finish).find('.name'))
+		setHeight($advantages.slice(start, finish).find('.desc'))
+
+		start = start + step
+		finish = finish + step
+	})
+}
 
 // Выравнивание преимуществ
-function advantageHeight(context, step){
-	let start       = 0,
-		finish      = step,
+function advantageHeight(context, step) {
+	let start = 0,
+		finish = step,
 		$advantages = context.find('.item')
 
 	$advantages.find('.name, .desc').height('auto')
 
-	$advantages.each(function(){
-		setHeight( $advantages.slice(start, finish).find('.name') )
-		setHeight( $advantages.slice(start, finish).find('.desc') )
+	$advantages.each(function () {
+		setHeight($advantages.slice(start, finish).find('.name'))
+		setHeight($advantages.slice(start, finish).find('.desc'))
 
-		start  = start + step
+		start = start + step
 		finish = finish + step
 	})
 }
 
 // Выравнивание Документов
-function documentHeight(context, step){
-	let start      = 0,
-		finish     = step,
+function documentHeight(context, step) {
+	let start = 0,
+		finish = step,
 		$documents = context.find('.document')
 
 	$documents.find('.name').height('auto')
 
-	$documents.each(function(){
-		setHeight( $documents.slice(start, finish).find('.name') )
+	$documents.each(function () {
+		setHeight($documents.slice(start, finish).find('.name'))
 
-		start  = start + step
+		start = start + step
 		finish = finish + step
 	})
 }
